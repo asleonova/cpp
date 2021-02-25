fe32e7a2237452ca45a03e63ac74bec13a731be3
Anna <asleonova.1@gmail.com>
2021-02-25 21:59:40 +0300

    some changes to ex00

--- D04/ex00/Sorcerer.hpp
+++ D04/ex00/Sorcerer.hpp
@@ -16,7 +16,7 @@ class Sorcerer
         ~Sorcerer();
         std::string getName() const;
         std::string getTitle() const;
-        virtual void polymorph(Victim const & victim) const;
+        void polymorph(Victim const & victim) const;
 };
 
 std::ostream & operator<<(std::ostream & o, Sorcerer const & rhs);
